ae04722da6e004e346831630d862e3337d864a3f
abrandfonbrener <abrandfonbrener23@amherst.edu>
2020-10-13 15:42:01 -0400

    Alex had completed his graphic

--- PUG_Shiny_App.R
+++ PUG_Shiny_App.R
@@ -6,19 +6,17 @@ path_in <- "C:/Users/abran/OneDrive/Documents/STAT 231 - Data Science/git/Shiny-
 data <- read_csv(paste0(path_in,"/shiny_project_data.csv"))
 
 
-data2 <- data %>% 
-  mutate(school_name = as.factor(school_name)
-         )
 
 
 
-
+names <- c("Academic Expectations", "Communication", "Engagement", "Safety and Respect",
+           "Total Satisfaction")
 
 
 # ui 
 ui <- fluidPage(
   
-  h1("Comparing Standardized Testing Scores and Student, Parent and Teacher Satisfaction"),
+  h1("Comparing Standardized Testing Scores and School Satisfaction"),
     
   
       
@@ -27,19 +25,19 @@ ui <- fluidPage(
                            
                            selectInput(inputId = "school", 
                                        label = "Choose a school to highlight:",
-                                       choices = data2$school_name,
+                                       choices = data$school_name,
                                        selected = "Staten Island Yabc"),
+                           selectInput(inputId = "statistic", 
+                                       label = "Choose a statistic to compare to Average SAT Score:",
+                                       choices = names,
+                                       selected = "Total Satisfaction"),
                            plotOutput(outputId = "bivariate"), 
                   
                   
                            
                   
                   
-                 # selectInput(inputId = "statistic", 
-                          #    label = "Choose a statistic to compare to Average SAT Score:",
-                              
-                         #     choices = candy$brand,
-                          #    selected = "M&M's"),
+                 
                   ),
                   
                   tabPanel(title = "Univariate Box-and-Whisker", 
@@ -60,31 +58,49 @@ ui <- fluidPage(
 server <- function(input,output){
   
  
-  
-  output$bivariate <- renderPlot({
-    ggplot(data = data2, aes(x = SAT_score, y = total_satisfaction)) +
-      geom_point(aes(colour = case_when(school_name == input$school ~ TRUE,
-                                      school_name != input$school ~ FALSE
-      )
-      )
-      ) + 
-      theme(legend.position = "none"
-      ) + 
-      labs(
-        title = "Association between Average SAT Score and Selected Satisfaction Statistic",
-        x = "Average SAT Score (out of 2400)"
-      ) + 
-      scale_fill_manual(values = c("#D35400", "#6E2C00"))
+  data_bivariate <- reactive({
+    mutate(data, y = case_when(input$statistic == "Academic Expectations" ~ data$academic_expectations,
+                               input$statistic == "Communication" ~ data$communication,
+                               input$statistic == "Engagement" ~ data$engagement,
+                               input$statistic == "Safety and Respect" ~ data$safety_and_respect,
+                               input$statistic == "Total Satisfaction" ~ data$total_satisfaction
+                               )
+          )
   })
   
   
-  
-  
+  output$bivariate <- renderPlot({
+    ggplot(data = data_bivariate(), aes(x = SAT_score, y = y
+                                        )
+           ) +
+      geom_point(aes(colour = case_when(school_name == input$school ~ TRUE,
+                                      school_name != input$school ~ FALSE
+                                        ),
+                     alpha = case_when(school_name == input$school ~ TRUE,
+                                        school_name != input$school ~ FALSE
+                                      ),
+                     size = case_when(school_name == input$school ~ TRUE,
+                                      school_name != input$school ~ FALSE
+                                      )
+                    )
+                ) + 
+      theme(legend.position = "none"
+            ) + 
+      labs(
+        x = "Average SAT Score (out of 2400)",
+        y = input$statistic
+          ) + 
+      scale_fill_manual(values = c("#D35400", "#6E2C00")
+                        ) +
+      scale_alpha_discrete(range = c(1, 1)
+                           ) +
+      scale_size_discrete(range = c(2, 5)
+                          )
+      
     
-    
-    
- 
-}
+  }) # Bivariate plot
+
+} # server
 
 # call to shinyApp
 shinyApp(ui = ui, server = server)
