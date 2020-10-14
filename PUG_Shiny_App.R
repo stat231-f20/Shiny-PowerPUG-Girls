c2b5f71923317423399daaee71aee03135f78f01
abrandfonbrener <abrandfonbrener23@amherst.edu>
2020-10-14 19:25:47 -0400

    Grace's Code

--- PUG_Shiny_App.R
+++ PUG_Shiny_App.R
@@ -12,6 +12,19 @@ data <- read_csv(paste0(path_in,"/shiny_project_data.csv"))
 names <- c("Academic Expectations", "Communication", "Engagement", "Safety and Respect",
            "Total Satisfaction")
 
+variables <- c("SAT Reading Score"
+               , "SAT Math Score"
+               , "SAT Writing Score"
+               , "Parent Response Rate"
+               , "Teacher Response Rate"
+               , "Student Response Rate"
+               , "Academic Expectations"
+               , "Communication"
+               , "Engagement"
+               , "Safety and Respect"
+               , "Total SAT Score"
+               , "Total Satisfaction")
+
 
 # ui 
 ui <- fluidPage(
@@ -36,12 +49,16 @@ ui <- fluidPage(
                   
                            
                   
-                  
+  
                  
                   ),
                   
                   tabPanel(title = "Univariate Box-and-Whisker", 
-                           #plotOutput(outputId = "univariate")
+                           selectInput(inputId = "variable",
+                                       label = "Choose a variable to compare to School Type",
+                                       choices = variables,
+                                       selected = "Total SAT Score"),
+                           plotOutput(outputId = "univariate")
                   ),
                   tabPanel(title = "Map", 
                            #plotOutput(outputId = "map")
@@ -66,6 +83,23 @@ server <- function(input,output){
                                input$statistic == "Total Satisfaction" ~ data$total_satisfaction
                                )
           )
+  })
+  
+  data_univariate <- reactive({
+    mutate(data, y = case_when(input$variable == "SAT Reading Score" ~ data$reading,
+                               input$variable == "SAT Math Score" ~ data$math,
+                               input$variable == "SAT Writing Score" ~ data$writing,
+                               input$variable == "Parent Response Rate" ~ data$parent_response_rate,
+                               input$variable == "Teacher Response Rate" ~ data$teacher_response_rate,
+                               input$variable == "Student Response Rate" ~ data$student_response_rate,
+                               input$variable == "Academic Expectations" ~ data$academic_expectations,
+                               input$variable == "Communication" ~ data$communication,
+                               input$variable == "Engagement" ~ data$engagement,
+                               input$variable == "Safety and Respect" ~ data$safety_and_respect,
+                               input$variable == "Total Satisfaction" ~ data$total_satisfaction,
+                               input$variable == "Total SAT Score" ~ data$SAT_score
+    )
+    )
   })
   
   
@@ -100,6 +134,18 @@ server <- function(input,output){
     
   }) # Bivariate plot
 
+  #univariate plot
+  output$univariate <- renderPlot({
+    data_univariate() %>%
+      mutate(class = fct_reorder(school_type, SAT_score, .fun='median')) %>%
+      ggplot(aes(x=reorder(school_type, y), y=y)) + 
+      geom_boxplot(color = 'red') +
+      xlab("class") +
+      theme(legend.position="none") +
+      xlab("")
+    
+  }) 
+  
 } # server
 
 # call to shinyApp
